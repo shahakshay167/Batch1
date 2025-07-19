d4bf128baf774d7abb1615c6cea952095f271405
renuka <renukakahane9@gmail.com>
2025-07-19 18:36:09 +0530

    add files

---
+++ cal.js
@@ -0,0 +1,45 @@
+class calculator{
+    constructor(a,b){
+        this.a=a;
+        this.b=b;
+    }
+    add(){
+        return this.a+this.b
+;    }
+    Subtract(){
+    return this.a-this.b
+;    }
+    Multiply(){
+    return this.a*this.b
+;    }
+    Division(){
+    return this.a/this.b
+;    }
+}
+
+function calculate(){
+    var p = Number(document.getElementById("val1").value);
+    var q = Number(document.getElementById("val2").value);
+    var choice = document.getElementById("optr").value;
+
+
+let obj = new calculator(p,q);
+switch (choice) {
+    case'+':{
+        document.getElementById("ans").innerHTML = obj.add();
+        break;
+    }
+    case'-':{
+        document.getElementById("ans").innerHTML = obj.Subtract();
+        break;
+    }
+    case'*':{
+        document.getElementById("ans").innerHTML = obj.Multiply();
+        break;
+    }
+    case'/':{
+        document.getElementById("ans").innerHTML = obj.Division();
+        break;
+    }
+}
+}
