62f08a054fb7f0423c5bbb41391beeaaeb96aeb7
renuka <renukakahane9@gmail.com>
2025-07-23 19:08:04 +0530

    add files

---
+++ firstapp/src/App.js
@@ -0,0 +1,11 @@
+import Akshay from './components/Demo11';
+import logo from './logo.svg';
+function App() {
+  return (
+    <div className="App">
+    <Akshay Name='Akshay'/>
+    </div>
+  );
+}
+
+export default App;
---
+++ firstapp/src/components/Demo11.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import '../components/CSS/Demo.css';
+
+
+const imgobj = [
+  {
+    id:1,
+    imgName:'images1',
+    imgpath:'images.jpg'
+  },
+  {
+    id:1,
+    imgName:'images2',
+    imgpath:'akshay5.png'
+  }
+]
+
+function Akshay(props) {
+  return (
+    <div>
+        <h1>Akshay</h1>
+        <h1>Vivek</h1>
+        <h3>My Name is {props.Name}</h3>
+        {
+          imgobj.map((item,index)=>(
+              <img key={index} className='img' src={item.imgpath} alt={item.imgName}/>
+          ))
+          }
+    </div>
+  );
+}
+
+export default Akshay;
